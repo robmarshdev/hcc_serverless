e68636d16ec8a0c35586b624d6bfb69217ce2565
Robert Marsh <rob.dmind@gmail.com>
2019-02-21 19:48:41 +0000

    import from multiple cinemas

--- src/movie-importer.ts
+++ src/movie-importer.ts
@@ -1,10 +1,13 @@
 import { get } from "superagent";
 import { formatUrlDate } from "../lib/format-url-date";
+import CINEMAS from "../utils/consts/cinemas";
 
-const importMovies = async () => {;
-  const req = await get(`https://www.findanyfilm.com/api/screenings/by_venue_id/venue_id/10719/date-from/${formatUrlDate(new Date())}`);
-  const body = req.body;
-  console.log("Result: ", JSON.stringify(body["10719"].films));
+const importMovies = async () => {
+  for (const cinemaId of CINEMAS) {
+    const req = await get(`https://www.findanyfilm.com/api/screenings/by_venue_id/venue_id/${cinemaId}/date-from/${formatUrlDate(new Date())}`);
+    const body = req.body;
+    console.log(`Cinema ${cinemaId} result: `, JSON.stringify(body[cinemaId].films));
+  }
 };
 
 export const handler = (event, context) => importMovies();
---
+++ utils/consts/cinemas.ts
@@ -0,0 +1,6 @@
+export default [
+  "10647", // Reel
+  "10719", // Odeon Luxe
+  "9669", // Vue
+  "7523", // Cineworld
+];
